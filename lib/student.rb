e39ad0806df773de985838687cceb003574d1430
Leifur Sigurdsson <leifurerik@gmail.com>
2018-11-15 00:05:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,10 +42,10 @@ class Student
   end
 
   def self.students_below_12th_grade
-    sql <<-SQL
+    sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade >= 11
+      WHERE grade <= 11
     SQL
 
     DB[:conn].execute(sql).map do |row|
